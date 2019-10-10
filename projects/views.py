645a59089fdde475aecc5d837245b9bbd067484a
osbornnick <nicholas.p.osborn@gmail.com>
2019-10-10 17:00:54 -0400

    added views.project_detail()

--- projects/views.py
+++ projects/views.py
@@ -8,4 +8,11 @@ def project_index(request):
         'projects': projects
     }
     return render(request, 'project_index.html', context)
-# Create your views here.
+
+
+def project_detail(request, pk):
+    project = Project.objects.get(pk=pk)
+    context = {
+        'project': project
+    }
+    return render(request, 'project_detail.html', context)
