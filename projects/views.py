a7b84d5e610b9676c6d319f33dc3d9883ccaa621
osbornnick <nicholas.p.osborn@gmail.com>
2019-10-10 16:55:09 -0400

    init commit

---
+++ projects/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from projects.models import Project
+
+
+def project_index(request):
+    projects = Project.objects.all()
+    context = {
+        'projects': projects
+    }
+    return render(request, 'project_index.html', context)
+# Create your views here.
