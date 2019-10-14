eeb48bd6756044b4ed4ae206626e1280d73f9f4f
osbornnick <nicholas.p.osborn@gmail.com>
2019-10-14 09:10:32 -0400

    now.sh config

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -78,12 +78,14 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = {}
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
